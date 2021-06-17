5c95a0825529392d563f843908fc3bc13a0a2c9f
Vishal Chaudhary <59690373+wingspiker@users.noreply.github.com>
2021-06-17 18:20:07 +0530

    commit

--- myshoppingcart/settings.py
+++ myshoppingcart/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-mzz51iys6dd_t_l!yzclh%2-gm$=ob(&i%(a66noi+f42id*l$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1','']
+ALLOWED_HOSTS = ['127.0.0.1','myshoppingcaart.herokuapp.com']
 
 
 # Application definition
